47a4b1a6bcb4063464673ca9448638fd21312b1e
mjkgarrow <mjkgarrow@gmail.com>
2022-12-04 21:40:01 +1100

    changed key input into ascii numbers

--- app.py
+++ app.py
@@ -7,26 +7,30 @@ def main(window):
 
     while True:
         # Get the inputted key from user
-        key = window.getkey()
+        key = window.getch()
 
         # Check if key is 'esc'
-        if key == '^[':
+        if key == 27:
             quit()
 
         # Check if key is alphanumeric/punctuation
-        elif 32 < ord(key) < 126:
+        elif 32 < key < 126:
             # TODO - this is the main block of code
+            window.addstr(chr(key))
             pass
 
         # Check if key is 'delete'
-        elif key == 'KEY_BACKSPACE':
+        elif key == 127:
             # TODO - how to delete
             pass
 
         # Check if key is 'enter'
-        elif key == 'KEY_ENTER':
+        elif key == 13:
             # TODO - how to declare game over
             pass
+
+        # Display new content
+        window.refresh()
 
 
 if __name__ == '__main__':
