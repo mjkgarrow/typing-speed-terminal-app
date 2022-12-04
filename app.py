ce9d2695b6468e28171c2c6d9df77b54c85343cb
mjkgarrow <mjkgarrow@gmail.com>
2022-12-04 21:28:45 +1100

    skeleton of loop to get user input

--- app.py
+++ app.py
@@ -4,7 +4,29 @@ import os
 
 
 def main(window):
-    pass
+
+    while True:
+        # Get the inputted key from user
+        key = window.getkey()
+
+        # Check if key is 'esc'
+        if key == '^[':
+            quit()
+
+        # Check if key is alphanumeric/punctuation
+        elif 32 < ord(key) < 126:
+            # TODO - this is the main block of code
+            pass
+
+        # Check if key is 'delete'
+        elif key == 'KEY_BACKSPACE':
+            # TODO - how to delete
+            pass
+
+        # Check if key is 'enter'
+        elif key == 'KEY_ENTER':
+            # TODO - how to declare game over
+            pass
 
 
 if __name__ == '__main__':
