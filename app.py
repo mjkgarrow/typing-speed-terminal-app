20d46d27f82042010b53b8c8d40812cdbf20cfe5
mjkgarrow <mjkgarrow@gmail.com>
2022-12-06 00:11:45 +1100

    wip user input comparing to prompt

--- app.py
+++ app.py
@@ -13,42 +13,27 @@ TEXT_START_X = int((WINDOW_SIZE[0] - MAX_WIDTH)//2)
 TEXT_START_Y = int(WINDOW_SIZE[1] * 0.2)
 
 
-def set_shorter_esc_delay_in_os():
-    '''Triggers escape key immediately rather than waiting a second'''
-    os.environ.setdefault("ESCDELAY", "25")
-
-
-def check_key_validity(window):
-    while True:
-        # Get the inputted key from user
-        key = window.getch()
-
-        # Quit if key is 'esc'
-        if key == 27:
-            quit()
-
-        # Check if key is alphanumeric/punctuation and return it
-        elif 32 <= key < 126:
-            return chr(key)
-
-
-def load_file(window):
+def load_file():
+    '''Loads txt file from within app directory, returns a string of file contents'''
     directory = os.getcwd()
-    for file in os.listdir(directory):
-        if file.endswith(".txt") and file != "requirements.txt":
-            with open(file, 'r') as f:
-                lines = f.read().splitlines()
-                return " ".join(lines)
+    while True:
+        for file in os.listdir(directory):
+            if file.endswith(".txt") and file != "requirements.txt":
+                with open(file, 'r') as f:
+                    lines = f.read().splitlines()
+                    return " ".join(lines)
 
 
 def quick_print(window, x, y, text):
-    window.clear()
+    ''' Clears screen and types supplied text'''
+    window.erase()
     window.addstr(
         y, x, text)
     window.refresh()
 
 
 def menu(window, x, y):
+    ''' Displays menu for user to select an option, returns a typing prompt based on the option'''
     while True:
         menu_text = ["Welcome to Speed-Typer, a typing game to test your skills",
                      "Please select from the following options:",
@@ -57,7 +42,7 @@ def menu(window, x, y):
                      "3. Test a quote",
                      "4. Quit"]
 
-        window.clear()
+        window.erase()
 
         for i in range(len(menu_text)):
             window.addstr(y + i, x, menu_text[i])
@@ -68,8 +53,8 @@ def menu(window, x, y):
             case 49:
                 quick_print(
                     window, x, y, "Copy '.txt' file to app directory to load text")
-                file_text = load_file(window)
-                return [file_text]
+                file_text = load_file()
+                return textwrap.wrap(file_text, MAX_WIDTH)
             case 50:
                 quick_print(
                     window, x, y, "Loading words from MIT")
@@ -85,7 +70,7 @@ def menu(window, x, y):
                 for i in range(50):
                     word_selection.append(
                         str(random_words[random.randint(0, len(random_words))])[2:-1])
-                return [" ".join(word_selection)]
+                return textwrap.wrap(" ".join(word_selection), MAX_WIDTH)
             case 51:
                 quick_print(
                     window, x, y, "Loading quotes from Type.fit API")
@@ -96,7 +81,7 @@ def menu(window, x, y):
                     quick_print(window, x, y, "Unable to load quotes, sorry!")
                     continue
                 quote = response[random.randint(0, len(response))]
-                return [f"{quote['text']} - {quote['author']}"]
+                return textwrap.wrap(f"{quote['text']} - {quote['author']}", MAX_WIDTH)
             case 52:
                 quick_print(
                     window, x, y, "Goodbye")
@@ -104,16 +89,27 @@ def menu(window, x, y):
                 quit()
 
 
-def main(window):
+def draw(window, text):
+    '''Draws text to the screen, line by line'''
+    window.addstr(0, 0, "Press 'esc' to exit, 'enter' to return to menu")
+    for i in range(len(text)):
+        window.addstr(TEXT_START_Y + i, TEXT_START_X,
+                      text[i])
+    # window.move(TEXT_START_Y, TEXT_START_X)
 
-    # Ask user for menu choice
+
+def main(window):
+    # Set cursor to invisible
+    curses.curs_set(0)
+
+    # Ask user for menu choice, return a typing prompt
     typing_prompt = menu(window, TEXT_START_X, TEXT_START_Y)
 
     # Update delay so that program isn't waiting on user input
     window.nodelay(True)
 
     # Clear screen of menu
-    window.clear()
+    window.erase()
 
     # Applying text styling colours to variables
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
@@ -123,6 +119,9 @@ def main(window):
     green = curses.color_pair(2)
     red = curses.color_pair(3)
 
+    # Variable to store user input
+    user_typed_string = ""
+
     while True:
         # Get the inputted key from user
         try:
@@ -130,41 +129,34 @@ def main(window):
         except:
             key = None
 
-        # Check if key is 'esc'
+        # Check if key is 'esc', quits immediately
         if key == 27:
             quit()
 
-        # Check if key is alphanumeric/punctuation
+        # Check if 'enter' key hit, clears input and returns to main menu
+        if key == 10 or key == 13:
+            user_typed_string = ""
+            typing_prompt = menu(window, TEXT_START_X, TEXT_START_Y)
+
+            # Check if key is alphanumeric/punctuation, add to user input
         elif 32 <= key < 126:
-            # TODO - this is the main block of code
-            if len(output) < MAX_WIDTH:
-                sub_output += chr(key)
-            else:
-                USER_KEYPRESS.append(chr(key))
-                sub_output = chr(key)
-                # Check if key is 'delete'
+            user_typed_string += chr(key)
+
+        # Check if key is backspace, remove from user input
         elif key == 127:
             try:
-                USER_KEYPRESS.pop()
-            except IndexError:
+                user_typed_string = user_typed_string[:-1]
+            except:
                 continue
 
-        # Check if key is 'enter'
-        elif key == 10:
-            # TODO - how to declare game over
-            quit()
+        # Draw typing prompt to screen
+        draw(window, typing_prompt)
 
-        # Clear window, ready for updated content
-        window.clear()
+        # Make text wrapped so it will fit in center of screen
+        wrapped_user_typed = textwrap.wrap(user_typed_string, MAX_WIDTH)
 
-        # Generate output, separating lines so it fits in center
-
-        for index in range(len(PROMPT)):
-            sub_output += PROMPT[index]
-
-        # Draw output
-        for i in range(len(output)):
-            window.addstr(TEXT_START_X + i, TEXT_START_Y, output[i])
+        # Draw user input
+        draw(window, wrapped_user_typed)
 
         # Display new content
         window.refresh()
@@ -172,7 +164,7 @@ def main(window):
 
 if __name__ == "__main__":
     # Sets delay on escape key to 25 milliseconds
-    set_shorter_esc_delay_in_os()
+    os.environ.setdefault("ESCDELAY", "25")
 
     # Calls the main function
     curses.wrapper(main)
