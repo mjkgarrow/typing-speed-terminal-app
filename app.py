00f9602966605796875b050d576bafd2dce25aab
mjkgarrow <mjkgarrow@gmail.com>
2022-12-05 20:53:44 +1100

    wip main menu

--- app.py
+++ app.py
@@ -1,33 +1,113 @@
 import curses
 import time
 import os
+import textwrap
+import requests
+import random
 
-
-USER_KEYPRESS = []
-PROMPT = []
+MENU_OPTION = 0
+USER_KEYPRESS = ""
+PROMPT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempor nulla id finibus maximus. Sed eget efficitur dolor, in sodales massa. Donec vitae condimentum lectus, ut scelerisque ex. Vivamus aliquet aliquam diam eu hendrerit. Donec et mauris non arcu aliquet elementum. Aliquam ultrices ac sem non ornare. Donec bibendum pharetra lorem, ut ullamcorper nisi rhoncus at. In eu lacus non tortor interdum varius. Suspendisse aliquam ex eu dignissim dictum. Morbi non cursus dui. Praesent auctor nisi vitae est iaculis tempus. Aliquam scelerisque convallis lorem ut aliquam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae."
 
 
 def set_shorter_esc_delay_in_os():
     '''Triggers escape key immediately rather than waiting a second'''
-    os.environ.setdefault('ESCDELAY', '25')
+    os.environ.setdefault("ESCDELAY", "25")
 
 
-def main(window):
-
-    window_size = [os.get_terminal_size()[0], os.get_terminal_size()[1]]
-    max_width = int(window_size[0]//1.3)
-    text_start = int((window_size[0] - max_width)//2)
-
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
-    WHITE_COLOUR = curses.color_pair(1)
-    GREEN_COLOUR = curses.color_pair(2)
-    RED_COLOUR = curses.color_pair(3)
-
+def check_key_validity(window):
     while True:
         # Get the inputted key from user
         key = window.getch()
+
+        # Quit if key is 'esc'
+        if key == 27:
+            quit()
+
+        # Check if key is alphanumeric/punctuation and return it
+        elif 32 <= key < 126:
+            return chr(key)
+
+
+def load_file(window):
+    file_path = ""
+    if os.path.exists(file_path):
+        with open(file_path, 'r', encoding='utf-8-sig') as f:
+            return f.read().splitlines()
+    else:
+        return 1
+
+
+def quick_print(window, x, y, text):
+    window.clear()
+    window.addstr(
+        y, x, text)
+    window.refresh()
+
+
+def menu(window, x, y):
+    while True:
+        menu_text = ["Welcome to Speed-Typer, an typing game to test your skills",
+                     "Please select from the following options:",
+                     "1. Test from file",
+                     "2. Test random words",
+                     "3. Test a quote",
+                     "5. Quit"]
+
+        key = window.getch()
+
+        for i in range(len(menu_text)):
+            window.addstr(y + i, x, menu_text[i])
+
+        window.refresh()
+
+        match key:
+            case 27:
+                quit()
+            case 49:
+                quick_print(
+                    window, x, y, "To load file, drag file to 'in-file folder")
+                file_text = load_file(window)
+                if file_text != 1:
+                    return file_text
+            case 50:
+                try:
+                    response = requests.get(
+                        "https://www.mit.edu/~ecprice/wordlist.10000")
+                WORDS = response.content.splitlines()
+                word_selection = []
+                for i in range(200):
+                    word_selection.append(WORDS[random.random(0, len(WORDS))])
+                return word_selection
+
+
+def main(window):
+    window.nodelay(True)
+
+    # Finding the terminal window size to correctly center text
+    window_size = [os.get_terminal_size()[0], os.get_terminal_size()[1]]
+    max_width = int(window_size[0]//1.3)
+    text_start_x = int((window_size[0] - max_width)//2)
+    text_start_y = int(window_size[1] * 0.2)
+
+    # Applying text styling colours to variables
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+    white = curses.color_pair(1)
+    green = curses.color_pair(2)
+    red = curses.color_pair(3)
+
+    response = menu(window, text_start_x, text_start_y)
+
+    output = []
+    sub_output = ''
+    while True:
+        # Get the inputted key from user
+        try:
+            key = window.getch()
+        except:
+            key = None
 
         # Check if key is 'esc'
         if key == 27:
@@ -36,9 +116,12 @@ def main(window):
         # Check if key is alphanumeric/punctuation
         elif 32 <= key < 126:
             # TODO - this is the main block of code
-            USER_KEYPRESS.append(chr(key))
-
-        # Check if key is 'delete'
+            if len(output) < max_width:
+                sub_output += chr(key)
+            else:
+                USER_KEYPRESS.append(chr(key))
+                sub_output = chr(key)
+                # Check if key is 'delete'
         elif key == 127:
             try:
                 USER_KEYPRESS.pop()
@@ -50,30 +133,23 @@ def main(window):
             # TODO - how to declare game over
             quit()
 
+        # Clear window, ready for updated content
         window.clear()
 
-        # Generate output onto screen, separating lines so it fits in center
-        output = [str(max_width), 1]
-        sub_output = ''
-        for index in range(len(USER_KEYPRESS)):
-            sub_output += str(index)
-            # if index % max_width != 0:
-            #     sub_output += USER_KEYPRESS[index]
-            # else:
-            #     sub_output += USER_KEYPRESS[index]
-            #     output.append(sub_output)
-            #     sub_output = ''
+        # Generate output, separating lines so it fits in center
+
+        for index in range(len(PROMPT)):
+            sub_output += PROMPT[index]
 
         # Draw output
-        # window.addstr(0, text_start, output[0])
         for i in range(len(output)):
-            window.addstr(i, text_start, output[0])
+            window.addstr(text_start_x + i, text_start_y, output[i])
 
         # Display new content
         window.refresh()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Sets delay on escape key to 25 milliseconds
     set_shorter_esc_delay_in_os()
 
